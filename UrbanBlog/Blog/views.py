b9836cf06be93b74dff2b35cc80b608922e9121b
YaEd71 <yaeduard71@gmail.com>
2024-10-18 10:00:53 +0300

    19_5

---
+++ UrbanBlog/Blog/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from .models import Post
+
+# Create your views here.
+
+def post_list(request):
+    posts = Post.objects.all().order_by('-created_at')
+
+    posts_per_page = request.GET.get('posts_per_page', 10)
+    paginator = Paginator(posts, int(posts_per_page))
+
+    page_number = request.GET.get('page')
+    try:
+        page_posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        page_posts = paginator.page(1)
+    except EmptyPage:
+        page_posts = paginator.page(paginator.num_pages)
+
+    return render(request, 'post_list.html', {
+        'page_posts': page_posts,
+        'posts_per_page': posts_per_page
+    })
