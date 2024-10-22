64be1de6211498c0bf89e280baa9dd7a637ce762
YaEd71 <yaeduard71@gmail.com>
2024-10-22 11:13:47 +0300

    19_6

--- UrbanBlog/Blog/models.py
+++ UrbanBlog/Blog/models.py
@@ -7,3 +7,20 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    age = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    publication_date = models.DateField()
+    isbn = models.CharField(max_length=13, unique=True)
+    authors = models.ManyToManyField(Author, related_name='books')
+
+    def __str__(self):
+        return self.title
